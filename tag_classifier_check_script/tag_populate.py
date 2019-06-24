5539d7ac45f95b563fe3e78b899a803ff1436bb1
Nilansh Bansal <bansalnilansh@gmail.com>
2019-06-24 23:07:46 +0530

    mary thangvell timeline tweet tags

--- tag_classifier_check_script/tag_populate.py
+++ tag_classifier_check_script/tag_populate.py
@@ -3,36 +3,33 @@ import os
 import csv 
 import json
 
-with open('twitter_timeline.csv','r') as f:
+with open('tweets.csv','r') as f:
 	reader = csv.reader(f)
 	messages = []
 	old_tags = []
 	for row in reader:
 		messages.append(row[1])
-		old_tags.append(row[2])
 
 url = 'http://68.183.89.148:9090/trial'
 count = 0 
 all_data = []
-for idx,message in enumerate(messages[1:1500],start=1):
+for idx,message in enumerate(messages[1:],start=1):
 	count += 1
 	obj = {}
 	r = requests.post(url,data={'sentdata[q]':message})
 	res_json = json.loads(r.json()['filters'])
 	obj['s.no'] = count
-	obj['new_tags'] = res_json['parents'] + res_json['children']
-	print(obj['new_tags'])
-	obj['msg'] = message
-	obj['old_tags'] = old_tags[idx]
+	obj['tags'] = res_json['parents'] + res_json['children']
+	obj['tweets'] = message
 	all_data.append(obj)
 
 
 writeHeader=True
-if os.path.exists('twitter_dataset_1500.csv') :
+if os.path.exists('devrel_tweets.csv') :
 	writeHeader=False
 
-headlines = ['s.no','msg','old_tags','new_tags']
-with open('twitter_dataset_1500.csv', 'w',encoding='utf-8-sig',newline='') as f:
+headlines = ['s.no','tweets','tags']
+with open('devrel_tweets.csv', 'w',encoding='utf-8-sig',newline='') as f:
 	writer = csv.DictWriter(f,headlines)
 	if writeHeader:
 		writer.writeheader()
