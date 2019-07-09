823790216460f31e610c874397b3756f093973b7
Nilansh Bansal <bansalnilansh@gmail.com>
2019-07-09 11:42:11 +0530

    version2_analysis

--- tag_classifier_check_script/script.py
+++ tag_classifier_check_script/script.py
@@ -1,41 +1,48 @@
-import requests
-import os
-import csv 
+"""
+Created on Tue July 09 11:29:14 2019
+@author: nilanshbansal
+"""
 import json
+import csv
+import os
+import requests
 
-with open('twitter_timeline.csv','r') as f:
-	reader = csv.reader(f)
-	messages = []
-	old_tags = []
-	for row in reader:
-		messages.append(row[1])
-		old_tags.append(row[2])
+
+with open('devrel_tweets.csv', 'r') as f:
+    reader = csv.reader(f)
+    messages = []
+    version1_tags = []
+    suggested_tags = []
+    for row in reader:
+        messages.append(row[0])
+        version1_tags.append(row[1])
+        suggested_tags.append(row[2])
 
 url = 'http://68.183.89.148:9090/trial'
-count = 0 
+count = 0
 all_data = []
-for idx,message in enumerate(messages[1:1500],start=1):
-	count += 1
-	obj = {}
-	r = requests.post(url,data={'sentdata[q]':message})
-	res_json = json.loads(r.json()['filters'])
-	obj['s.no'] = count
-	obj['new_tags'] = res_json['parents'] + res_json['children']
-	print(obj['new_tags'])
-	obj['msg'] = message
-	obj['old_tags'] = old_tags[idx]
-	all_data.append(obj)
+for idx, message in enumerate(messages[1:], start=1):
+    count += 1
+    obj = {}
+    r = requests.post(url, data={'sentdata[q]': message})
+    res_json = json.loads(r.json()['filters'])
+    obj['s.no'] = count
+    obj['version2_tags'] = res_json['TOPIC'] + res_json['SUBTOPIC']
+    obj['Tweet'] = message
+    obj['version1_tags'] = version1_tags[idx]
+    obj['suggested_tags'] = suggested_tags[idx]
+    all_data.append(obj)
 
 
-writeHeader=True
-if os.path.exists('twitter_dataset_1500.csv') :
-	writeHeader=False
+writeHeader = True
+if os.path.exists('twitter_devrel_analysis_version2.csv'):
+    writeHeader = False
 
-headlines = ['s.no','msg','old_tags','new_tags']
-with open('twitter_dataset_1500.csv', 'w',encoding='utf-8-sig',newline='') as f:
-	writer = csv.DictWriter(f,headlines)
-	if writeHeader:
-		writer.writeheader()
-	for data in all_data:
-		writer.writerow(data)
-
+headlines = ['s.no', 'Tweet', 'version1_tags',
+             'version2_tags', 'suggested_tags']
+with open('twitter_devrel_analysis_version2.csv', 'w', encoding='utf-8-sig', newline='') as f:
+    writer = csv.DictWriter(f, headlines)
+    if writeHeader:
+        writer.writeheader()
+    for data in all_data:
+        writer.writerow(data)
